f3a65866f7206a37418de560be69b4459383b14a
Xander Geldof <xander.geldof@student.howest.be>
2025-04-04 16:29:53 +0200

    Resolved conflict

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -68,7 +68,6 @@ public class UIMainMenu : MonoBehaviour
 
         _masterVCA.setVolume(value);
         _masterVCA.getVolume(out _masterVCAVolume);
-        
     }
 
     public void SFXSliderChanged(float value)

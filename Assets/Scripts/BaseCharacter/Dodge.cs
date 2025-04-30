a0bd67d5c2c5088b05f8213376ca5f49b4e0657a
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-04-30 15:58:15 +0200

    made it so player will dash in his forward direction when there is no movement input

--- Assets/Scripts/BaseCharacter/Dodge.cs
+++ Assets/Scripts/BaseCharacter/Dodge.cs
@@ -49,7 +49,9 @@ public class Dodge : MonoBehaviour
 
         _loseStamina.Raise(this, new StaminaEventArgs { StaminaCost = _staminaCost.value });
 
-        _dashDirection = new Vector3(_moveInput.Value.x, 0, _moveInput.Value.y);
+        if (_moveInput.Value != Vector2.zero)
+            _dashDirection = new Vector3(_moveInput.Value.x, 0, _moveInput.Value.y);
+        else _dashDirection = transform.forward;
         _dashing = true;
         _canRun = false;
         StartCoroutine(EndDodge());

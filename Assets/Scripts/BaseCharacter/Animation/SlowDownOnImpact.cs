9dbb2d70890b3edf057dd1261178ff0cb24204b6
TGoncpp <141250817+TGoncpp@users.noreply.github.com>
2025-05-22 12:05:42 +0200

    made a nice async lerping

---
+++ Assets/Scripts/BaseCharacter/Animation/SlowDownOnImpact.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SlowDownOnImpact : MonoBehaviour
+{
+    [SerializeField, Tooltip("The time duration the animation will be slowed down")] private float _slowedDuration = 0.1f;
+    [Range(0f, 0.5f)]
+    [SerializeField, Tooltip("The set animationspeed for during slow down")] private float _slowSpeed = 0.1f;
+    [Range(0f, 0.5f)]
+    [SerializeField, Tooltip("The time percentage duration the animation will ease in or out")] private float _easInPercentage = 0.2f; 
+    [SerializeField, Tooltip("The animator parameter name")] private string _animParameter = "ActionSpeed";
+
+    private Animator _animator;
+    private float _currentSpeed = 1f;
+    private float _easInDuration = 0.2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        if (_animator == null)
+            Debug.LogError("no animator found in Slowdown component");
+        _easInDuration = _slowedDuration * _easInPercentage;
+    }
+
+    public async void StartSlowDown(Component sender, object obj)
+    {
+        AttackEventArgs args = obj as AttackEventArgs;
+        if (args == null || args.Attacker != gameObject) return;
+
+        float start = _animator.GetFloat(_animParameter);
+        float miliseconds = (_slowedDuration - 2f * _easInDuration) * 1000f;
+
+        await EaseAnimSpeed(start, _slowSpeed, _easInDuration);
+        await Task.Delay(Mathf.RoundToInt(miliseconds));
+        await EaseAnimSpeed(_slowSpeed, start, _easInDuration);
+    }
+
+    private async Task EaseAnimSpeed(float start, float end, float duration)
+    {
+        float elapsedTime = 0f;
+
+        while(elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime/duration);
+
+            float easedValue = t < 0.5f ?
+                2 * t * t :
+                1f - Mathf.Pow(-2f * t + 2f, 2f) * 0.5f;
+
+            float value = Mathf.Lerp(start, end, easedValue);
+            _animator.SetFloat(_animParameter, value);
+            await Task.Yield(); 
+        }
+
+        _animator.SetFloat(_animParameter, end);
+    }
+
+
+}
